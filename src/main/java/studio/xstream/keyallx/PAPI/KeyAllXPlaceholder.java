73f623ee2569c4f02102e37462ec92dad234fff9
Ammar <123098233+Ammar-XD@users.noreply.github.com>
2024-04-30 16:39:56 +0530

    Added more placeholders!

--- src/main/java/studio/xstream/keyallx/PAPI/KeyAllXPlaceholder.java
+++ src/main/java/studio/xstream/keyallx/PAPI/KeyAllXPlaceholder.java
@@ -38,23 +38,33 @@ public class KeyAllXPlaceholder extends PlaceholderExpansion {
 
     @Override
     public String onPlaceholderRequest(Player player, String identifier) {
-        if (identifier.equals("timer")) {
-            long timerIntervalInSeconds = plugin.timeInterval;
-            long days = TimeUnit.SECONDS.toDays(timerIntervalInSeconds);
-            long hours = TimeUnit.SECONDS.toHours(timerIntervalInSeconds) % 24;
-            long minutes = TimeUnit.SECONDS.toMinutes(timerIntervalInSeconds) % 60;
-            long seconds = timerIntervalInSeconds % 60;
+        long timerIntervalInSeconds = plugin.timeInterval;
+        long days = TimeUnit.SECONDS.toDays(timerIntervalInSeconds);
+        long hours = TimeUnit.SECONDS.toHours(timerIntervalInSeconds) % 24;
+        long minutes = TimeUnit.SECONDS.toMinutes(timerIntervalInSeconds) % 60;
+        long seconds = timerIntervalInSeconds % 60;
 
-            if (days > 0) {
-                return String.format("%d:%02d:%02d:%02d", days, hours, minutes, seconds);
-            } else if (hours > 0) {
-                return String.format("%02d:%02d:%02d", hours, minutes, seconds);
-            } else if (minutes > 0) {
-                return String.format("%02d:%02d", minutes, seconds);
-            } else {
-                return String.format("%02d", seconds);
-            }
+        switch (identifier.toLowerCase()) {
+            case "timer":
+                if (days > 0) {
+                    return String.format("%d:%02d:%02d:%02d", days, hours, minutes, seconds);
+                } else if (hours > 0) {
+                    return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+                } else if (minutes > 0) {
+                    return String.format("%02d:%02d", minutes, seconds);
+                } else {
+                    return String.format("%02d", seconds);
+                }
+            case "days":
+                return String.valueOf(days);
+            case "hours":
+                return String.valueOf(hours);
+            case "mins":
+                return String.valueOf(minutes);
+            case "secs":
+                return String.valueOf(seconds);
+            default:
+                return null;
         }
-        return null;
     }
 }
